1a4f6b02a4d8454a117fa5bd14fe48fbd477511c
Shafaq Nawaz <shafaqnawaz784@gmail.com>
2022-07-17 20:35:13 +0500

    All changes in rorapp

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # root 'application#hello'
+  #root route ko jis controller ko send karna h uss 
+  #controller ka naam before _ likh do
+  root 'pages#home' #home is the action/function defined in teh controller
 end
