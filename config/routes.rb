8180986418b53427747ceacc9536120d8ec9be96
Shafaq Nawaz <shafaqnawaz784@gmail.com>
2022-07-18 09:25:25 +0500

    Making the home page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   #root route ko jis controller ko send karna h uss 
   #controller ka naam before _ likh do
   root 'pages#home' #home is the action/function defined in teh controller
+  get 'about', to:'pages#about'
+
 end
